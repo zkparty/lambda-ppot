9831f793f362529b281f40f1e6ab50b1bee39e5e
Nico Serrano <nnicosp@hotmail.com>
2023-03-11 13:14:37 -0500

    confirm-email(fix): code distribution in functions

--- src/handlers/confirm-email.ts
+++ src/handlers/confirm-email.ts
@@ -3,7 +3,7 @@ import {
     APIGatewayProxyResult
 } from "aws-lambda";
 import { verify } from 'jsonwebtoken';
-import { UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { PutCommandOutput, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 
 import { ddbDocClient } from "../dynamo";
 import { BodyResponse, Payload } from "../types";
@@ -25,32 +25,36 @@ export const confirmEmailHandler = async (event: APIGatewayProxyEvent): Promise<
         const { token } = event.queryStringParameters;
         const { email } = verify(token, JWT_PRIVATE_KEY) as Payload;
 
-        // add email to the blacklist for a while
-        const paramsToBlacklist = {
-            TableName: EMAILS_TABLE,
-            Key: { "email": email },
-            UpdateExpression: 'ADD #expiration :set',
-            ExpressionAttributeNames: { '#expiration': 'expiration' },
-            ExpressionAttributeValues: { ':set': Math.floor(Date.now() / 1000) + TIME_TO_EXPIRE_CONFIRMED_EMAIL }, //  86400 = 24 hours
-            ReturnValues: 'ALL_NEW',
-        };
-        const blacklist = await ddbDocClient.send(new UpdateCommand(paramsToBlacklist));
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                registered: true,
-                ...blacklist,
-            } as BodyResponse)
-        }
+        // add to blacklist for a while
+        const blacklist = await addToBlacklist(email);
+        return createResponse({...blacklist}, true);
+
     } catch (error) {
         // catch any error and return information about it
         console.log("Error: ", error);
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                registered: false,
-                ...error,
-            } as BodyResponse)
-        }
+        return createResponse({...error}, false);
     }
+}
+
+function createResponse(data: any, registered: boolean): APIGatewayProxyResult {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            blacklisted: registered,
+            ...data,
+        } as BodyResponse)
+    }
+}
+
+async function addToBlacklist(email: string): Promise<PutCommandOutput>{
+    const paramsToBlacklist = {
+        TableName: EMAILS_TABLE,
+        Key: { "email": email },
+        UpdateExpression: 'ADD #expiration :set',
+        ExpressionAttributeNames: { '#expiration': 'expiration' },
+        ExpressionAttributeValues: { ':set': Math.floor(Date.now() / 1000) + TIME_TO_EXPIRE_CONFIRMED_EMAIL }, //  86400 = 24 hours
+        ReturnValues: 'ALL_NEW',
+    };
+    const blacklist = await ddbDocClient.send(new UpdateCommand(paramsToBlacklist));
+    return blacklist;
 }
