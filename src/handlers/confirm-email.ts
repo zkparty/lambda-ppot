f2b88d2b35ca52aaeabacde72843c48a0ef58896
Nico Serrano <nnicosp@hotmail.com>
2023-03-11 11:06:07 -0500

    confirm-email(feat): set global envs + WIP JWT verify

--- src/handlers/confirm-email.ts
+++ src/handlers/confirm-email.ts
@@ -2,25 +2,32 @@ import {
     APIGatewayProxyEvent,
     APIGatewayProxyResult
 } from "aws-lambda";
-import { BodyResponse } from "../types";
-
+import { verify } from 'jsonwebtoken';
 import { PutCommand } from "@aws-sdk/lib-dynamodb";
 
 import { ddbDocClient } from "../dynamo";
-import { EMAILS_TABLE, TIME_TO_EXPIRE_CONFIRMED_EMAIL } from "../constants";
+import { BodyResponse } from "../types";
+import {
+    EMAILS_TABLE,
+    JWT_PRIVATE_KEY,
+    TIME_TO_EXPIRE_CONFIRMED_EMAIL
+} from "../constants";
 
 
 /*
  * Receive token from email and confirm validity
 */
 export const confirmEmailHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    if (event.httpMethod !== 'POST') {
-        throw new Error(`requestRetrieval only accepts POST method, you tried: ${event.httpMethod} method.`);
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`confirmEmail only accepts GET method, you tried: ${event.httpMethod} method.`);
     }
     try {
         // TODO: get request data from auth token
-        const { email } = JSON.parse(event.body);
-        // add email to the blacklist for a while
+        // TODO: cannot destructure this
+        const { token } = event.queryStringParameters;
+        const save = verify(token, JWT_PRIVATE_KEY);
+
+        /*// add email to the blacklist for a while
         const paramsToAdd = {
             TableName: EMAILS_TABLE,
             Item: {
@@ -28,12 +35,13 @@ export const confirmEmailHandler = async (event: APIGatewayProxyEvent): Promise<
                 expiration: Math.floor(Date.now() / 1000) + TIME_TO_EXPIRE_CONFIRMED_EMAIL, //  86400 = 24 hours
             }
         }
-        const save = await ddbDocClient.send(new PutCommand(paramsToAdd));
+        const save = await ddbDocClient.send(new PutCommand(paramsToAdd));*/
         return {
             statusCode: 200,
             body: JSON.stringify({
                 registered: true,
-                ...save,
+                save: JSON.stringify(save),
+                //...save,
             } as BodyResponse)
         }
     } catch (error) {
