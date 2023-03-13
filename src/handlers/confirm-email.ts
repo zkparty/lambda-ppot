11d0e08d6f281afc278fc73ab2caac5ef57d392b
Nico Serrano <nnicosp@hotmail.com>
2023-03-12 23:22:56 -0500

    s3(feat): WIP restore glacier file

--- src/handlers/confirm-email.ts
+++ src/handlers/confirm-email.ts
@@ -3,11 +3,21 @@ import {
     APIGatewayProxyResult
 } from "aws-lambda";
 import { verify } from 'jsonwebtoken';
-import { PutCommandOutput, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import {
+    PutCommandOutput,
+    UpdateCommand
+} from "@aws-sdk/lib-dynamodb";
+import {
+    S3Client,
+    RestoreObjectCommand,
+    RestoreObjectCommandOutput,
+    RestoreObjectCommandInput
+} from "@aws-sdk/client-s3";
 
 import { ddbDocClient } from "../dynamo";
 import { BodyResponse, Payload } from "../types";
 import {
+    REGION,
     EMAILS_TABLE,
     JWT_PRIVATE_KEY,
     TIME_TO_EXPIRE_CONFIRMED_EMAIL
@@ -25,10 +35,12 @@ export const confirmEmailHandler = async (event: APIGatewayProxyEvent): Promise<
         const { token } = event.queryStringParameters;
         const { email } = verify(token, JWT_PRIVATE_KEY) as Payload;
 
+        // TODO: if user is in blacklist then only respond: blacklisted: true ? what if it is requesting other file?
         // add to blacklist for a while
         const blacklist = await addToBlacklist(email);
         // TODO: start moving file from S3 Glacier to S3 Standard
-        return createResponse({...blacklist}, true);
+        const restore = await restoreGlacierObject();
+        return createResponse({blacklist, restore}, true);
 
     } catch (error) {
         // catch any error and return information about it
@@ -45,6 +57,24 @@ function createResponse(data: any, blacklisted: boolean): APIGatewayProxyResult 
             ...data,
         } as BodyResponse)
     }
+}
+
+async function restoreGlacierObject(): Promise<RestoreObjectCommandOutput> {
+    const s3Client = new S3Client({ region: REGION });
+    const paramsToRestore = {
+        Bucket: 'perpetual-powers-of-tau',
+        Key: 'challenges/current_state.txt',
+        RestoreRequest: {
+            Days: 1,
+            GlacierJobParameters: {
+                Tier: 'Standard'
+            },
+            Tier: 'Standard'
+        },
+    } as RestoreObjectCommandInput;
+    const restore = await s3Client.send( new RestoreObjectCommand(paramsToRestore) );
+    s3Client.destroy();
+    return restore;
 }
 
 async function addToBlacklist(email: string): Promise<PutCommandOutput>{
