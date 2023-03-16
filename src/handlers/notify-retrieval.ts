52b3b5c9b369433f672d71307c843c900c982dd9
Nico Serrano <nnicosp@hotmail.com>
2023-03-16 01:30:46 -0500

    notify(docs): comments

--- src/handlers/notify-retrieval.ts
+++ src/handlers/notify-retrieval.ts
@@ -2,11 +2,12 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { BodyResponse } from "../types";
 
 /**
- * Notify email uer that file has been retrieved and it is ready to use
+ * Notify email user that file has been retrieved and it is ready to use
  */
 export const notifyRetrievalHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
         // TODO: get it is an internal AWS S3 event
+        // https://docs.aws.amazon.com/lambda/latest/dg/with-s3-example.html
         // TODO: get users that requested file
         // TODO: notify users
         return createResponse({}, true);
